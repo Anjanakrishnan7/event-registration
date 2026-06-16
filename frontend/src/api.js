8bd0c694d89882cf0d07cbfd735347dc59c0fd45
Anjanakrishnan7 <anjanakrishnananil@gmail.com>
2026-06-16 13:07:24 +0530

    Update API URL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://event-registration-backend-782k.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
